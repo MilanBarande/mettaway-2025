bb4548139582142b7eb75f7d05dfdf31df3998b2
Milan <milan.barande@gmail.com>
2025-10-08 17:36:23 +0200

    fix payment info

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,16 +5,16 @@ export const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 // Payment Information
 export const PAYMENT_INFO = {
-  iban: process.env.PAYMENT_IBAN,
+  iban: process.env.NEXT_PUBLIC_PAYMENT_IBAN,
   accountHolder: "appricot GmbH",
   address: "Industriestrasse 70, 6300 Zug, Switzerland",
   revolut: {
     name: "Lukas Hotz",
-    phone: process.env.PAYMENT_PHONE,
+    phone: process.env.NEXT_PUBLIC_PAYMENT_PHONE,
   },
   twint: {
     link: "https://go.twint.ch/1/e/tw?tw=acq.N_syTGB2S22l8iFj2bOfdrVCi15qZNvseICaSQyvpc0ZmjpIn1MPE6CKOzOenu7T.",
   },
-  contactEmail: process.env.CONTACT_EMAIL,
+  contactEmail: process.env.NEXT_PUBLIC_CONTACT_EMAIL,
 } as const;
 
